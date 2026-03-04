92651a2e21babb0242530a475329cfba7bf354d0
maulanaysfi <maulanayusuf5579@gmail.com>
2026-03-04 14:38:22 +0700

    Refined bash script

--- configure.sh
+++ configure.sh
@@ -10,24 +10,47 @@ OUTPUT_FILES=(
 )
 
 main() {
-  read -p "[*] Management IPv4 (with CIDR, ex: 10.10.10.231/24): " MANAGEMENT_IPV4_CIDR
-  read -p "[*] Management Gateway IPv4 (ex: 10.10.10.254): " MANAGEMENT_GTW
-  read -p "[*] Management DNS Address (default: 1.1.1.1): " MANAGEMENT_DNS
+  read -e -p "[*] Management IPv4 (with CIDR, ex: 192.168.1.23/24): " MANAGEMENT_IPV4_CIDR
+  MANAGEMENT_IPV4="${MANAGEMENT_IPV4_CIDR%/*}"
+  MANAGEMENT_GTW_TMP="${MANAGEMENT_IPV4%.*}.254"
+  read -e -p "[*] Management Gateway IPv4 (default: $MANAGEMENT_GTW_TMP): " MANAGEMENT_GTW
+  
+  if [[ -z "$MANAGEMENT_GTW" ]]; then
+    echo -e "[!] Management Gateway Address has been set to default."
+    MANAGEMENT_GTW="$MANAGEMENT_GTW_TMP"
+  fi
+
+  read -e -p "[*] Management DNS Address (default: 1.1.1.1): " MANAGEMENT_DNS
   
   if [[ -z "$MANAGEMENT_DNS" ]]; then
     echo -e "[!] Management DNS Address has been set to default."
     MANAGEMENT_DNS="1.1.1.1"
   fi
   
-  read -p "[*] Workload IPv4 (with CIDR, ex: 10.10.20.231/24): " WORKLOAD_IPV4_CIDR
-  read -p "[*] Workload Gateway IPv4 (ex: 10.10.20.254): " WORKLOAD_GTW
-  read -p "[*] Frontend IPv4 (with CIDR, ex: 10.10.30.231/24): " FRONTEND_IPV4_CIDR
-  read -p "[*] Frontend Gateway IPv4 (ex: 10.10.30.254): " FRONTEND_GTW
-  read -p "[*] Frontend Virtual IP Network CIDR (ex: 10.10.30.192/28): " FRONTEND_VIP_CIDR
-
-  MANAGEMENT_IPV4="${MANAGEMENT_IPV4_CIDR%/*}"
+  read -e -p "[*] Workload IPv4 (with CIDR, ex: 10.10.20.231/24): " WORKLOAD_IPV4_CIDR
   WORKLOAD_IPV4="${WORKLOAD_IPV4_CIDR%/*}"
+  WORKLOAD_GTW_TMP="${WORKLOAD_IPV4%.*}.254"
+
+  read -e -p "[*] Workload Gateway IPv4 (default: $WORKLOAD_GTW_TMP): " WORKLOAD_GTW
+  
+  if [[ -z "$WORKLOAD_GTW" ]]; then
+    echo -e "[!] Workload Gateway Address has been set to default."
+    WORKLOAD_GTW="$WORKLOAD_GTW_TMP"
+  fi
+
+  read -e -p "[*] Frontend IPv4 (with CIDR, ex: 10.10.30.231/24): " FRONTEND_IPV4_CIDR
   FRONTEND_IPV4="${FRONTEND_IPV4_CIDR%/*}"
+  FRONTEND_GTW_TMP="${FRONTEND_IPV4%.*}.254"
+
+  read -e -p "[*] Frontend Gateway IPv4 (default: $FRONTEND_GTW_TMP): " FRONTEND_GTW
+  
+  if [[ -z "$FRONTEND_GTW" ]]; then
+    echo -e "[!] Frontend Gateway Address has been set to default."
+    FRONTEND_GTW="$FRONTEND_GTW_TMP"
+  fi
+
+  read -e -p "[*] Frontend Virtual IP Network CIDR (ex: 10.10.30.192/28): " FRONTEND_VIP_CIDR
+
   
   # label if empty
   vars="MANAGEMENT_IPV4_CIDR MANAGEMENT_DNS MANAGEMENT_GTW WORKLOAD_IPV4_CIDR WORKLOAD_GTW FRONTEND_IPV4_CIDR FRONTEND_GTW FRONTEND_VIP_CIDR MANAGEMENT_IPV4 WORKLOAD_IPV4 FRONTEND_IPV4"
@@ -42,17 +65,19 @@ main() {
   
   echo ""
   echo "[!] Review your configuration below."
-  echo "MANAGEMENT_IPV4_CIDR:   $MANAGEMENT_IPV4_CIDR"
+  echo "MANAGEMENT_IPV4:        $MANAGEMENT_IPV4"
+  echo "WORKLOAD_IPV4:          $WORKLOAD_IPV4"
+  echo "FRONTEND_IPV4:          $FRONTEND_IPV4"
+  echo ""
   echo "MANAGEMENT_DNS:         $MANAGEMENT_DNS"
+  echo "MANAGEMENT_IPV4_CIDR:   $MANAGEMENT_IPV4_CIDR"
   echo "MANAGEMENT_GTW:         $MANAGEMENT_GTW"
   echo "WORKLOAD_IPV4_CIDR:     $WORKLOAD_IPV4_CIDR"
   echo "WORKLOAD_GTW:           $WORKLOAD_GTW"
   echo "FRONTEND_IPV4_CIDR:     $FRONTEND_IPV4_CIDR"
   echo "FRONTEND_GTW:           $FRONTEND_GTW"
+  echo ""
   echo "FRONTEND_VIP_CIDR:      $FRONTEND_VIP_CIDR"
-  echo "MANAGEMENT_IPV4:        $MANAGEMENT_IPV4"
-  echo "WORKLOAD_IPV4:          $WORKLOAD_IPV4"
-  echo "FRONTEND_IPV4:          $FRONTEND_IPV4"
   echo ""
 
   while true; do
